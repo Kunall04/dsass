0d4ee489d64793473f85092356cac2ff765689b0
Kunal Rohilla <93761501+Kunall04@users.noreply.github.com>
2025-11-01 01:48:15 +0530

    grp

---
+++ graph/bfs.cpp
@@ -0,0 +1,24 @@
+class Solution {
+  public:
+    vector<int> bfs(vector<vector<int>> &adj) {
+        int n=adj.size();
+        vector<int> visited(n,0);
+        visited[0]=1;
+        vector<int> bfs;
+        queue<int> q;
+        
+        q.push(0);
+        while(!q.empty()) {
+            int node=q.front(); q.pop();
+            bfs.push_back(node);
+            
+            for(auto it: adj[node]) {
+                if(!visited[it]) {
+                    visited[it]=1;
+                    q.push(it);
+                }
+            }
+        }
+        return bfs;
+    }
+};

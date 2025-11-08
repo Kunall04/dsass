ae886852ba8c1cd1b258d63e3ffa0c844c4c8500
Kunal Rohilla <93761501+Kunall04@users.noreply.github.com>
2025-11-08 23:21:38 +0530

    8th

---
+++ tree/leftview.cpp
@@ -0,0 +1,12 @@
+class Solution {
+  public:
+    vector<int> bfs(vector<vector<int>> &adj) {
+        int n=adj.size();
+        vector<int> visited(n,0);
+        visited[0]=1;
+        vector<int> bfs;
+        queue<int> q;
+        
+        
+    }
+};

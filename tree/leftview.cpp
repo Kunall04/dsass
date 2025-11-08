7c8970655eb71144ee4aa6a65d8ac3e39f3225cd
Kunal Rohilla <93761501+Kunall04@users.noreply.github.com>
2025-11-08 23:22:32 +0530

    8th

--- tree/leftview.cpp
+++ tree/leftview.cpp
@@ -7,6 +7,17 @@ class Solution {
         vector<int> bfs;
         queue<int> q;
         
-        
+        q.push(0);
+        while(!q.empty()) {
+            int node=q.front(); q.pop();
+            bfs.push_back(node);
+            
+            for(auto it: adj[node]) {
+                if(!visited[it]) {
+                    visited[it]=1;
+                    q.push(it);
+                }
+            }
+        }
     }
 };

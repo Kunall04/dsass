50698d8ed084d2c4209bcb33be14e752a893aa26
Kunal Rohilla <93761501+Kunall04@users.noreply.github.com>
2025-11-13 14:33:54 +0530

    sda

--- special/wordBreak.cpp
+++ special/wordBreak.cpp
@@ -1,35 +1,45 @@
-class Solution {
-public:
-    void solve(int i, string& s,string& w,unordered_set<string>& st, vector<string>& ans) {
-        if(i==s.size()) {
-            ans.push_back(w);
-            return;
-        }
+#include<bits/stdc++.h>
+using namespace std;
 
-        for(int j=i;j<s.size();j++) {
-            string sub=s.substr(i,j-i+1);
-            if(st.count(sub)) {
-                string prev=w;
-                if(!w.empty()) w+=" ";
-
-                w+=sub;
-                solve(j+1,s,w,st,ans);
-
-                // w.pop_back();
-                w=prev;
-            }
-        }
+void solve(int i, string& s,string& w,unordered_set<string>& st, vector<string>& ans) {
+    if(i==s.size()) {
+    ans.push_back(w);
+    return;
     }
 
-    vector<string> wordBreak(string s, vector<string>& words) {
-        unordered_set<string> st;
+    for(int j=i;j<s.size();j++) {
+        string sub=s.substr(i,j-i+1);
+        if(st.count(sub)) {
+            string prev=w;
+            if(!w.empty()) w+=" ";
 
-        for(const auto& word: words) st.insert(word);
+            w+=sub;
+            solve(j+1,s,w,st,ans);
 
-        vector<string> ans;
-        string w;
-        solve(0,s,w,st,ans);
-
-        return ans;
+            w=prev;
+        }
     }
-};
+}
+
+vector<string> wordBreak(string s, vector<string>& words) {
+    unordered_set<string> st;
+
+    for(const auto& word: words) st.insert(word);
+
+    vector<string> ans;
+    string w;
+    solve(0,s,w,st,ans);
+
+    return ans;
+}
+
+int main() {
+    string s="catsanddog";
+    vector<string> words={"cat","cats","and","sand","dog"};
+    
+    vector<string> ans=wordBreak(s,words);
+
+    for(const auto& line: ans) cout<<line<<'\n';
+
+    return 0;
+}
